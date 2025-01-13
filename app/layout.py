f6c4f47039b65dafc342458e22eeaec3567be0b2
bfc782 <bfc782@hotmail.com>
2025-01-13 08:00:06 +0000

    set visible components by tablename

--- app/layout.py
+++ app/layout.py
@@ -1,5 +1,6 @@
 from dash import dcc, html
 import dash_bootstrap_components as dbc
+from .ui_layer import tbl_to_layout_comps
 
 def serve_layout(model_components, tbl_cls_cols):
         return html.Div(
@@ -11,6 +12,7 @@ def serve_layout(model_components, tbl_cls_cols):
                             )]
                         + 
                         [getattr(model_components, f'{tbl}{comp}')
-                            for tbl in tbl_cls_cols for comp in ['_btn', '_modal', '_table']
+                            for tbl in tbl_cls_cols for comp in tbl_to_layout_comps[tbl]
+                            # for tbl in tbl_cls_cols for comp in ['_btn', '_modal', '_table']
                         ]
                     )
---
+++ app/ui_layer.py
@@ -0,0 +1,10 @@
+# for tbl in tbl_cls_cols for comp in ['_btn', '_modal', '_table']
+# note that tbl is model table name, e.g. tables (lower cased plural)
+
+# config MODELS_TO_LOAD = ['User', 'Team', 'Roster']
+
+tbl_to_layout_comps = {
+    'users': ['_btn', '_modal'],
+    'teams': ['_btn', '_modal', '_table'],
+    'rosters': []
+}
