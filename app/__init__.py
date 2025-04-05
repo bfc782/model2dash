dee67e0803a877a625fb8aed21fe4af5bfbe8814
bfc782 <bfc782@hotmail.com>
2025-04-05 18:56:12 +0100

    login html is not working yet but dash app works and index page too

--- app/__init__.py
+++ app/__init__.py
@@ -20,9 +20,12 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
     from .api import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api/v1')
-
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
 
     # mail.init_app(app)
     # moment.init_app(app)
